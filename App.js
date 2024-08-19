6fddf88db31645a11ca98ce954e041712f323807
Grizzly Alex <34487541+alejosod@users.noreply.github.com>
2024-08-19 09:15:58 -0600

    corrected some dependencies

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import SampleScreen from 'screens/SampleScreen'
+import SampleScreen from './src/screens/SampleScreen'
 
 const Stack = createStackNavigator()
 
